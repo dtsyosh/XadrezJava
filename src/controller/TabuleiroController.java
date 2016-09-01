8b6dfb45c3730d6320f866f5a67c85240b05b287
yosh <yosh@localhost>
2016-09-01 11:49:23 -0400

    Corrigido bug em que as linhas eram invertidas com as colunas

--- src/controller/TabuleiroController.java
+++ src/controller/TabuleiroController.java
@@ -40,38 +40,38 @@ public class TabuleiroController {
 
         //Peoes
         for (int i = 1; i <= 8; i++) {
-            this.tabuleiro.getTabuleiro()[i][2].setPeca(new Peao("peao", "preto"));
-            this.tabuleiro.getTabuleiro()[i][7].setPeca(new Peao("peao", "branco"));
+            this.tabuleiro.getTabuleiro()[2][i].setPeca(new Peao("peao", "preto"));
+            this.tabuleiro.getTabuleiro()[7][i].setPeca(new Peao("peao", "branco"));
         }
 
         //cavalos
-        this.tabuleiro.getTabuleiro()[2][1].setPeca(new Cavalo("cavalo", "preto"));
-        this.tabuleiro.getTabuleiro()[7][1].setPeca(new Cavalo("cavalo", "preto"));
+        this.tabuleiro.getTabuleiro()[1][2].setPeca(new Cavalo("cavalo", "preto"));
+        this.tabuleiro.getTabuleiro()[1][7].setPeca(new Cavalo("cavalo", "preto"));
 
-        this.tabuleiro.getTabuleiro()[2][8].setPeca(new Cavalo("cavalo", "branco"));
-        this.tabuleiro.getTabuleiro()[7][8].setPeca(new Cavalo("cavalo", "branco"));
+        this.tabuleiro.getTabuleiro()[8][2].setPeca(new Cavalo("cavalo", "branco"));
+        this.tabuleiro.getTabuleiro()[8][7].setPeca(new Cavalo("cavalo", "branco"));
 
         //torres
-        this.tabuleiro.getTabuleiro()[8][1].setPeca(new Torre("torre", "preto"));
+        this.tabuleiro.getTabuleiro()[1][8].setPeca(new Torre("torre", "preto"));
         this.tabuleiro.getTabuleiro()[1][1].setPeca(new Torre("torre", "preto"));
 
-        this.tabuleiro.getTabuleiro()[1][8].setPeca(new Torre("torre", "branco"));
+        this.tabuleiro.getTabuleiro()[8][1].setPeca(new Torre("torre", "branco"));
         this.tabuleiro.getTabuleiro()[8][8].setPeca(new Torre("torre", "branco"));
 
         //bispos
-        this.tabuleiro.getTabuleiro()[3][1].setPeca(new Bispo("bispo", "preto"));
-        this.tabuleiro.getTabuleiro()[6][1].setPeca(new Bispo("bispo", "preto"));
+        this.tabuleiro.getTabuleiro()[1][3].setPeca(new Bispo("bispo", "preto"));
+        this.tabuleiro.getTabuleiro()[1][6].setPeca(new Bispo("bispo", "preto"));
 
-        this.tabuleiro.getTabuleiro()[3][8].setPeca(new Bispo("bispo", "branco"));
-        this.tabuleiro.getTabuleiro()[6][8].setPeca(new Bispo("bispo", "branco"));
+        this.tabuleiro.getTabuleiro()[8][3].setPeca(new Bispo("bispo", "branco"));
+        this.tabuleiro.getTabuleiro()[8][6].setPeca(new Bispo("bispo", "branco"));
         
         //rei
-        this.tabuleiro.getTabuleiro()[5][1].setPeca(new Rei("rei", "preto"));
-        this.tabuleiro.getTabuleiro()[5][8].setPeca(new Rei("rei", "branco"));
+        this.tabuleiro.getTabuleiro()[1][5].setPeca(new Rei("rei", "preto"));
+        this.tabuleiro.getTabuleiro()[8][5].setPeca(new Rei("rei", "branco"));
         
         //raina
-        this.tabuleiro.getTabuleiro()[4][1].setPeca(new Rainha("rainha", "preto"));
-        this.tabuleiro.getTabuleiro()[4][8].setPeca(new Rainha("rainha", "branco"));
+        this.tabuleiro.getTabuleiro()[1][4].setPeca(new Rainha("rainha", "preto"));
+        this.tabuleiro.getTabuleiro()[8][4].setPeca(new Rainha("rainha", "branco"));
 
         this.getTabuleiroInfo().repintar();
     }
